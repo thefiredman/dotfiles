4aa07255d998a5c1d0d7b0c2e71bc6670ae2b024
thefiredman <44515019+thefiredman@users.noreply.github.com>
2025-07-09 12:51:40 -0400

    add: steam

---
+++ hosts/perSystem/x86_64-linux/nwll/steam.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+let path = "/var/lib/steam";
+in {
+  users = {
+    groups."steam" = { };
+    users = {
+      "steam" = {
+        isSystemUser = true;
+        home = path;
+        group = "steam";
+        extraGroups = [ "wheel" "video" ];
+      };
+    };
+  };
+
+  environment.persistence."/nix/persist" = {
+    directories = [{
+      directory = path;
+      user = "steam";
+      group = "steam";
+      mode = "0775";
+    }];
+  };
+
+  programs.steam = {
+    enable = true;
+    package = pkgs.steam.override { extraEnv = { HOME = path; }; };
+    extraCompatPackages = [ pkgs.proton-ge-custom ];
+  };
+}
